8ed16afd7d4b30843a86ae0982e58f66e4b2f817
Harivansh Rathi <rathiharivansh@gmail.com>
2026-03-21 18:55:33 -0400

    canola!

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -1,33 +1,29 @@
 return {
-    'barrettruth/oil.nvim',
-    dependencies = {
-        'nvim-tree/nvim-web-devicons',
-        {
-            'malewicz1337/oil-git.nvim',
-            opts = {
-                show_ignored_files = false,
-                show_ignored_directories = false,
-                debounce_ms = 300,
-            },
-        },
+    {
+        'barrettruth/canola.nvim',
+        branch = 'canola',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        init = function()
+            vim.g.canola = {
+                columns = { 'icon', 'git_status' },
+                hidden = { enabled = false },
+                keymaps = {
+                    ['g?'] = { callback = 'actions.show_help', mode = 'n' },
+                    ['<CR>'] = 'actions.select',
+                    ['<C-v>'] = { callback = 'actions.select', opts = { vertical = true } },
+                    ['<C-x>'] = { callback = 'actions.select', opts = { horizontal = true } },
+                    ['<C-p>'] = 'actions.preview',
+                    ['<C-c>'] = { callback = 'actions.close', mode = 'n' },
+                    ['-'] = { callback = 'actions.parent', mode = 'n' },
+                    ['g.'] = { callback = 'actions.toggle_hidden', mode = 'n' },
+                },
+            }
+            map('n', '-', '<cmd>Canola<cr>')
+            map('n', '<leader>e', '<cmd>Canola<cr>')
+        end,
     },
-    opts = {
-        default_file_explorer = true,
-        columns = { 'icon' },
-        view_options = { show_hidden = true },
-        keymaps = {
-            ['g?'] = 'actions.show_help',
-            ['<CR>'] = 'actions.select',
-            ['<C-v>'] = 'actions.select_vsplit',
-            ['<C-x>'] = 'actions.select_split',
-            ['<C-p>'] = 'actions.preview',
-            ['<C-c>'] = 'actions.close',
-            ['-'] = 'actions.parent',
-            ['g.'] = 'actions.toggle_hidden',
-        },
+    {
+        'barrettruth/canola-collection',
+        dependencies = { 'barrettruth/canola.nvim' },
     },
-    init = function()
-        map('n', '-', '<cmd>Oil<cr>')
-        map('n', '<leader>e', '<cmd>Oil<cr>')
-    end,
 }

f3d61787f014cfb1ce8d6428f47a4361dae09bd6
Harivansh Rathi <rathiharivansh@gmail.com>
2026-03-29 17:49:27 -0400

    update treesitter config for v1.x API (configs module removed)

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -4,56 +4,51 @@ return {
         build = ':TSUpdate',
         dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
         config = function()
-            require('nvim-treesitter.configs').setup({
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ['af'] = '@function.outer',
-                            ['if'] = '@function.inner',
-                            ['ac'] = '@class.outer',
-                            ['ic'] = '@class.inner',
-                            ['aa'] = '@parameter.outer',
-                            ['ia'] = '@parameter.inner',
-                            ['ai'] = '@conditional.outer',
-                            ['ii'] = '@conditional.inner',
-                            ['al'] = '@loop.outer',
-                            ['il'] = '@loop.inner',
-                            ['ab'] = '@block.outer',
-                            ['ib'] = '@block.inner',
-                        },
+            require('nvim-treesitter-textobjects').setup({
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ['af'] = '@function.outer',
+                        ['if'] = '@function.inner',
+                        ['ac'] = '@class.outer',
+                        ['ic'] = '@class.inner',
+                        ['aa'] = '@parameter.outer',
+                        ['ia'] = '@parameter.inner',
+                        ['ai'] = '@conditional.outer',
+                        ['ii'] = '@conditional.inner',
+                        ['al'] = '@loop.outer',
+                        ['il'] = '@loop.inner',
+                        ['ab'] = '@block.outer',
+                        ['ib'] = '@block.inner',
                     },
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            [']f'] = '@function.outer',
-                            [']c'] = '@class.outer',
-                            [']a'] = '@parameter.inner',
-                        },
-                        goto_next_end = {
-                            [']F'] = '@function.outer',
-                            [']C'] = '@class.outer',
-                        },
-                        goto_previous_start = {
-                            ['[f'] = '@function.outer',
-                            ['[c'] = '@class.outer',
-                            ['[a'] = '@parameter.inner',
-                        },
-                        goto_previous_end = {
-                            ['[F'] = '@function.outer',
-                            ['[C'] = '@class.outer',
-                        },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        [']f'] = '@function.outer',
+                        [']c'] = '@class.outer',
+                        [']a'] = '@parameter.inner',
                     },
-                    swap = {
-                        enable = true,
-                        swap_next = { ['<leader>sn'] = '@parameter.inner' },
-                        swap_previous = { ['<leader>sp'] = '@parameter.inner' },
+                    goto_next_end = {
+                        [']F'] = '@function.outer',
+                        [']C'] = '@class.outer',
                     },
+                    goto_previous_start = {
+                        ['[f'] = '@function.outer',
+                        ['[c'] = '@class.outer',
+                        ['[a'] = '@parameter.inner',
+                    },
+                    goto_previous_end = {
+                        ['[F'] = '@function.outer',
+                        ['[C'] = '@class.outer',
+                    },
+                },
+                swap = {
+                    enable = true,
+                    swap_next = { ['<leader>sn'] = '@parameter.inner' },
+                    swap_previous = { ['<leader>sp'] = '@parameter.inner' },
                 },
             })
         end,

d5e0f9816c31818d31b17c81e4e8f2382cf3fba1
Harivansh Rathi <rathiharivansh@gmail.com>
2026-04-01 21:24:49 +0000

    smb hostname truncate + hard limit

--- modules/macos.nix
+++ modules/macos.nix
@@ -1,9 +1,27 @@
-{ ... }:
+{ config, ... }:
 {
   security.pam.services.sudo_local.touchIdAuth = true;
 
   # Karabiner-Elements is managed via Homebrew cask because nix-darwin's
   # built-in module is broken with 15.7+ (missing karabiner_grabber/observer binaries).
+
+  system.defaults.smb.NetBIOSName = builtins.substring 0 15 config.networking.hostName;
+  system.defaults.smb.ServerDescription = config.networking.hostName;
+
+  launchd.daemons."limit.maxfiles" = {
+    serviceConfig = {
+      Label = "limit.maxfiles";
+      ProgramArguments = [
+        "/bin/launchctl"
+        "limit"
+        "maxfiles"
+        "65536"
+        "200000"
+      ];
+      RunAtLoad = true;
+      KeepAlive = false;
+    };
+  };
 
   system.defaults = {
     dock.autohide = true;

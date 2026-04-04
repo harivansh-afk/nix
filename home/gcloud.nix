b8b3876ceb9fdedf31a9e46d08695f5abfe3dd08
Harivansh Rathi <rathiharivansh@gmail.com>
2026-04-04 20:16:05 +0000

    symlink gcloud file

--- home/gcloud.nix
+++ home/gcloud.nix
@@ -1,11 +1,13 @@
 { lib, ... }:
 {
-  xdg.configFile."gcloud/active_config".text = "default\n";
+  # Write gcloud config as mutable files (not Nix store symlinks) so that
+  # gcloud auth / gws auth can update them at runtime.
+  home.activation.gcloudConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    install -Dm644 /dev/null "$HOME/.config/gcloud/active_config"
+    printf 'default' > "$HOME/.config/gcloud/active_config"
 
-  xdg.configFile."gcloud/configurations/config_default".text = lib.generators.toINI { } {
-    core = {
-      account = "rathiharivansh@gmail.com";
-      project = "hari-gc";
-    };
-  };
+    install -Dm644 /dev/null "$HOME/.config/gcloud/configurations/config_default"
+    printf '[core]\naccount=rathiharivansh@gmail.com\nproject=hari-gc\n' \
+      > "$HOME/.config/gcloud/configurations/config_default"
+  '';
 }

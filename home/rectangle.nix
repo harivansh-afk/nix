21c947e4ae148caa5f4d339a220ed8daf7e7759e
Hari <73809867+harivansh-afk@users.noreply.github.com>
2026-04-15 13:44:48 -0400

    feat: aerospace (#73)

--- home/rectangle.nix
+++
@@ -1,6 +0,0 @@
-{ lib, ... }:
-{
-  home.activation.importRectanglePreferences = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    /usr/bin/defaults import com.knollsoft.Rectangle ${../config/rectangle/Rectangle.plist}
-  '';
-}

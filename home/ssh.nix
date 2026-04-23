7fbb52ebd39fcbb14d009aa2bbda1a3b7ce39136
Harivansh Rathi <rathiharivansh@gmail.com>
2026-04-22 21:21:05 -0400

    chore: re-instate netty ssh

--- home/ssh.nix
+++ home/ssh.nix
@@ -11,6 +11,13 @@
         identityFile = "~/.ssh/id_ed25519";
       };
 
+      netty = {
+        hostname = "152.53.195.59";
+        user = "rathi";
+        identityFile = "~/.ssh/netty";
+        identitiesOnly = true;
+      };
+
       spark = {
         hostname = "100.73.113.83";
         user = "rathi";

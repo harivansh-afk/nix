ff5644138ece9030591529e06590a5b6f69fa454
Harivansh Rathi <rathiharivansh@gmail.com>
2026-05-21 05:46:19 +0000

    Remove Cloudflare SSH path for Spark

--- modules/services/cloudflared.nix
+++ modules/services/cloudflared.nix
@@ -11,7 +11,6 @@ in
     tunnels.${tunnelId} = {
       credentialsFile = config.sops.secrets."cloudflared.json".path;
       default = "http://127.0.0.1:80";
-      ingress."spark.harivan.sh" = "ssh://127.0.0.1:22";
     };
   };
 }

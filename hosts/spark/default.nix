e81f97d6edbe5700c6e94684e620259d1a4c4cfd
Harivansh Rathi <rathiharivansh@gmail.com>
2026-06-28 18:17:59 +0000

    spark: mini-loops framework + x-life-scan loop + observability

--- hosts/spark/default.nix
+++ hosts/spark/default.nix
@@ -29,6 +29,7 @@
     ../../modules/services/kb-graph.nix
     ../../modules/services/kb-research.nix
     ../../modules/services/kb-finance.nix
+    ../../modules/services/mini-loops.nix
     ../../modules/services/mosh.nix
     ../../modules/services/parakeet.nix
     ../../modules/services/vaultwarden.nix

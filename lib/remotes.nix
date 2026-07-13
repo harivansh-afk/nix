b83ddca568ccee1baabd27f6282fd6702521ccdd
Harivansh Rathi <rathiharivansh@gmail.com>
2026-07-13 11:56:13 -0700

    mux: add compute host aliases

--- lib/remotes.nix
+++ lib/remotes.nix
@@ -10,4 +10,24 @@
   hari1 = {
     host = "hari-compute-1";
   };
+
+  dev1 = {
+    host = "dev-1";
+  };
+
+  hil1 = {
+    host = "hil-compute-1";
+  };
+
+  hil2 = {
+    host = "hil-compute-2";
+  };
+
+  vin1 = {
+    host = "vin-compute-1";
+  };
+
+  vin2 = {
+    host = "vin-compute-2";
+  };
 }

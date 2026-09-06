53e1254a8dd02061183469121f078d274245e768
Harivansh Rathi <rathiharivansh@gmail.com>
2026-09-05 20:49:54 -0400

    macbook: declare Logitech Options+ and its startup services

--- hosts/macbook/startup/default.nix
+++ hosts/macbook/startup/default.nix
@@ -8,6 +8,11 @@ let
     "Raycast Beta"
   ];
   plists = [
+    "com.logi.cp-dev-mgr.plist"
+    "com.logi.optionsplus.plist"
+    "com.logi.optionsplus.updater.plist"
+    "com.logitech.LogiRightSight.plist"
+    "com.logitech.LogiRightSight.Agent.plist"
     "limit.maxfiles.plist"
     "systems.determinate.nix-daemon.plist"
     "systems.determinate.nix-installer.nix-hook.plist"
